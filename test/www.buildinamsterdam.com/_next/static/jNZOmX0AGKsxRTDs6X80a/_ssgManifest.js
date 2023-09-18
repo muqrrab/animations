8592a4e9937dd2e7864af0294ae793da859e32f0
Muqrrab <muqrrab2@gmail.com>
2023-09-18 16:05:48 +0500

    first commit

---
+++ test/www.buildinamsterdam.com/_next/static/jNZOmX0AGKsxRTDs6X80a/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[pageSlug]","\u002Farticle\u002F[articleSlug]","\u002Fcase\u002F[caseSlug]","\u002Fcontact","\u002Fservices\u002F[serviceSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
